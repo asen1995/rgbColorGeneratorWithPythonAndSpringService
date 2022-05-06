898dd6eb4c93a25d8596e774fdd3d8e1a246d61c
Asen Nikolaev <asen.nikolaev95@abv.bg>
2022-05-06 17:41:37 +0300

    init empty rgbColorComponent

--- front-end/rgb-color-viewer-application/src/app/app.module.ts
+++ front-end/rgb-color-viewer-application/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RgbColorComponent } from './rgb-color/rgb-color.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RgbColorComponent
   ],
   imports: [
     BrowserModule
---
+++ front-end/rgb-color-viewer-application/src/app/rgb-color/rgb-color.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rgb-color',
+  templateUrl: './rgb-color.component.html',
+  styleUrls: ['./rgb-color.component.css']
+})
+export class RgbColorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
