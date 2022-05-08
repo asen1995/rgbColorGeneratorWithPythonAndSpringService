b2a77234b27cc9d27162b3e6986d0439c82451fc
Asen Nikolaev <asen.nikolaev95@abv.bg>
2022-05-08 21:41:26 +0300

    adding logs to track rest services

--- rgbCssPythonGeneratorApplication/service/HtmlGeneratorService.py
+++ rgbCssPythonGeneratorApplication/service/HtmlGeneratorService.py
@@ -5,5 +5,7 @@ htmlGeneratorAppURL = "http://localhost:8080/generateHtmlRgbDisplayCode"
 def callHtmlCodeGeneratorApp(rgbValue, text):
 
     request = {'rgbValue': rgbValue, 'text': text}
+
+    print('calling htmlGeneratorApp with request ' + str(request))
     response = requests.post(htmlGeneratorAppURL, json=request)
     return response
