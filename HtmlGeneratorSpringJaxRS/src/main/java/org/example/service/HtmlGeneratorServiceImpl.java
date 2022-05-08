fec57245aaf3892cf5c3aba175ad189c41fef1cb
Asen Nikolaev <asen.nikolaev95@abv.bg>
2022-05-08 16:19:14 +0300

    fix websocket sending from spring-> angular / by autowire SimpMessagingTemplate bean and call it manually instead of using the annotations

--- HtmlGeneratorSpringJaxRS/src/main/java/org/example/service/HtmlGeneratorServiceImpl.java
+++ HtmlGeneratorSpringJaxRS/src/main/java/org/example/service/HtmlGeneratorServiceImpl.java
@@ -3,29 +3,27 @@ package org.example.service;
 import org.example.model.HtmlWebSocketMessage;
 import org.example.model.RgbCodeRequest;
 import org.example.util.HtmlGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HtmlGeneratorServiceImpl implements HtmlGeneratorService {
+
+    @Autowired
+    WebSocketFrontEndClient webSocketFrontEndClient;
     @Override
     public String generateHtmlRgbDisplayCode(RgbCodeRequest request) throws Exception {
 
         String htmlCode = HtmlGenerator.generateDisplayColoredBoxWithText(request.getRgbValue(),request.getText());
-
         HtmlWebSocketMessage messageRequest = new HtmlWebSocketMessage(htmlCode);
 
-        //TODO send htmlcode via WebSocket
+        webSocketFrontEndClient.callWebSocketInFrontEnd(messageRequest);
 
-        greeting(messageRequest);
         return "SUCCESS";
     }
 
-    @MessageMapping("/hello")
-    @SendTo("/topic/greetings")
-    public HtmlWebSocketMessage greeting(HtmlWebSocketMessage messageRequest) throws Exception {
-        Thread.sleep(1000); // simulated delay
-        return messageRequest;
-    }
+
 }
---
+++ HtmlGeneratorSpringJaxRS/src/main/java/org/example/service/WebSocketFrontEndClientImpl.java
@@ -0,0 +1,18 @@
+package org.example.service;
+
+import org.example.model.HtmlWebSocketMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WebSocketFrontEndClientImpl implements WebSocketFrontEndClient {
+
+    @Autowired
+    SimpMessagingTemplate webSocketFrontEndSimpMessagingTemplate;
+
+    @Override
+    public void callWebSocketInFrontEnd(HtmlWebSocketMessage messageRequest) throws Exception {
+        this.webSocketFrontEndSimpMessagingTemplate.convertAndSend("/topic/greetings", messageRequest);
+    }
+}
