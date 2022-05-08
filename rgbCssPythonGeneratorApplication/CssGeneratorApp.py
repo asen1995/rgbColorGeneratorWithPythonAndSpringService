e69e6733374468f7cd9c15ef6557c3acdccbd17c
Asen Nikolaev <asen.nikolaev95@abv.bg>
2022-05-08 20:42:10 +0300

    python rest service : cors security adding headers in order to be able to be called from front-end

--- rgbCssPythonGeneratorApplication/CssGeneratorApp.py
+++ rgbCssPythonGeneratorApplication/CssGeneratorApp.py
@@ -6,6 +6,12 @@ from RgbRequestSchema import RgbRequestSchema
 
 app = Flask(__name__)
 
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  return response
 
 @app.route('/cssRgbGenerator', methods=['POST'])
 def cssRgbGenerator():
